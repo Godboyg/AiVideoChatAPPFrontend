53589858ba18c594899b63b1ca6a0116f3461d58
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-09 18:46:33 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,7 +12,7 @@ import { RiSendPlaneLine , RiCloseCircleLine , RiMessageFill, RiMessageLine} fro
 const API = import.meta.env.VITE_API_URL;
 console.log("api",API);
 const ENDPOINT = "https://aivediochatappbackend.onrender.com/";
-const socket = io(API , { withCredentials: true });
+const socket = io("https://aivediochatappbackend.onrender.com/" , { withCredentials: true });
 if(socket){
   console.log("socket connected",socket.id)
 }else{
