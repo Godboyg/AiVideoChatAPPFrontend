1a3406e61bed6bb22ee4902a9b2a91e5f03b49cd
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-10 22:42:38 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -246,70 +246,66 @@ function Home() {
     {
       authenticated ? (
         <div>
-      <div className="flex items-center justify-between py-3 px-5 max-sm:py-3 max-sm:px-4 bg-green-200 max-sm:bg-green-200">
-        <h2 className="font-bold text-xl">Soulmegal</h2>
-        <button className="p-3 bg-black rounded-xl text-white hover:cursor-pointer max-sm:mr-0" onClick={handleLogout}>LogOut</button>
-      </div>
-      <div className="flex">
-        <div className="">
-        <div className="flex max-sm:flex-col bg-black max-sm:gap-5 items-center justify-center ml-10 h-[60vh] overflow-hidden w-[65vw] max-sm:w-[70vw] max-sm:h-[55vh]">
-         <div className="flex items-center bg-green-500 justify-center w-full">
-           kushal
-           <video ref={videoRef} autoPlay muted playsInline className="w-full h-auto" />
-         </div>
-          <div className="flex items-center bg-green-800 justify-center w-full">
-            kushal
-           { remoteVideoRef ? (
-            <video ref={remoteVideoRef} autoPlay muted playsInline className="w-full h-auto" />
-           ) : (
-            <p className="mt-5">Waiting for other user...kushal.....</p>
-           )}
+          <div className="flex items-center justify-between py-3 px-5 max-sm:py-3 max-sm:px-4 bg-green-200 max-sm:bg-green-200">
+            <h2 className="font-bold text-xl">Soulmegal</h2>
+            <button className="p-3 bg-black rounded-xl text-white hover:cursor-pointer max-sm:mr-0" onClick={handleLogout}>LogOut</button>
           </div>
-         </div>
-        </div>
-        <div className="mt-5 ml-40 max-sm:ml-20 w-[20vw] max-sm:w-[60vw] flex max-sm:flex-wrap items-center justify-between">
-         <button onClick={startConnection} className="h-16 w-24 outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16">Start Call</button>
-         <button onClick={handleChangeUser} className="h-16 w-24 outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16">Change Call</button>
-         { partnerIdRef.current && <button className="h-16 w-24 max-sm:flex items-center justify-center outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16 hidden" onClick={() => setUserConnected(true)}><RiMessageLine /></button> }
-        </div>
-        </div>
-        <div className={`max-sm:w-full max-sm:absolute max-sm:p-7 w-full ${ userConnected ? "backdrop-blur-sm" : ""} `}>
-        {
-          userConnected ? (
+          <div className="flex">
+            <div className="">
+              <div className="flex max-sm:flex-col max-sm:gap-5 items-center justify-center ml-10 h-[60vh] overflow-hidden w-[65vw] max-sm:w-[70vw] max-sm:h-[55vh]">
+                <div className="flex items-center justify-center w-full">
+                  <video ref={videoRef} autoPlay muted playsInline className="w-full h-auto" />
+                </div>
+                <div className="flex items-center justify-center w-full">
+                  { remoteVideoRef ? (
+                    <video ref={remoteVideoRef} autoPlay muted playsInline className="w-full h-auto" />
+                  ) : (
+                    <p className="mt-5">Waiting for other user...kushal.....</p>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 ml-40 max-sm:ml-20 w-[20vw] max-sm:w-[60vw] flex max-sm:flex-wrap items-center justify-between">
+             <button onClick={startConnection} className="h-16 w-24 outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16">Start Call</button>
+             <button onClick={handleChangeUser} className="h-16 w-24 outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16">Change Call</button>
+             { partnerIdRef.current && <button className="h-16 w-24 max-sm:flex items-center justify-center outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16 hidden" onClick={() => setUserConnected(true)}><RiMessageLine /></button> }
+            </div>
+          </div>
+          <div className={`max-sm:w-full max-sm:absolute max-sm:p-7 w-full ${ userConnected ? "backdrop-blur-sm" : ""} `}>
+           {
+            userConnected ? (
             <>
             <p className="flex items-center justify-end mb-3"><RiCloseCircleLine size={30} onClick={() => setUserConnected(false)}/></p>
             <div className={`w-full h-[50vh] p-3 bg-green-300 shadow-2xl relative overflow-hidden ${ userConnected ? "scale-100 opacity-100" : ""}`}>
-          <p className="font-bold">Ai : {aiMessage}</p>
-          <div className="overflow-auto over">
-            <div className="w-full h-[40vh] bg-black">
-            {messages.map((msg, i) => (
-          <div key={i} className={`flex mt-3 ${msg.sender === socket.id ? "justify-end" : ""}`}>
-            <div
-              className={`p-2 max-w-xs rounded-xl ${
-                msg.sender === socket.id
-                  ? "bg-green-200 rounded-br-none"
-                  : "bg-white rounded-bl-none"
-              }`}
-            >
-              {msg.text}
-            </div>
-          </div>
-        ))}
-            </div>
-            <div className="w-full h-12 bg-black flex items-center justify-between rounded-lg absolute bottom-4 left-0">
-               <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} className="w-full h-full p-4 outline-none text-white" placeholder="Enter Message...."/>
-               <RiSendPlaneLine className="text-white mr-3" onClick={sendMessage} size={30}/>
-            </div>
-          </div>
+              <p className="font-bold">Ai : {aiMessage}</p>
+              <div className="overflow-auto over">
+               <div className="w-full h-[40vh] bg-black">
+                 {messages.map((msg, i) => (
+                   <div key={i} className={`flex mt-3 ${msg.sender === socket.id ? "justify-end" : ""}`}>
+                     <div
+                      className={`p-2 max-w-xs rounded-xl ${
+                      msg.sender === socket.id
+                      ? "bg-green-200 rounded-br-none"
+                      : "bg-white rounded-bl-none"
+                     }`}
+                    >
+                     {msg.text}
+                    </div>
+                   </div>
+                 ))}
+               </div>
+               <div className="w-full h-12 bg-black flex items-center justify-between rounded-lg absolute bottom-4 left-0">
+                 <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} className="w-full h-full p-4 outline-none text-white" placeholder="Enter Message...."/>
+                 <RiSendPlaneLine className="text-white mr-3" onClick={sendMessage} size={30}/>
+               </div>
+              </div>
             </div>
             </>
-          ) : (
-            ""
-          )
-        }
+            ) : (
+              ""
+           )}
+          </div>
         </div>
-      </div>
-    </div>
       ) : (
         <div className="w-full h-screen flex items-center bg-gray-500 justify-center">
           <p className="font-bold text-black">Loading............</p>
