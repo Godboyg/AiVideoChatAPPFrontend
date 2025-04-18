fbc54f8c884535cc35bf03a9f0dc4b9b64f34c1b
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-18 14:21:55 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -350,7 +350,7 @@ function Home() {
              { partnerIdRef.current && <button className="h-16 w-24 max-sm:flex items-center justify-center outline-none rounded-full hover:cursor-pointer bg-black text-white max-sm:text-[3vw] max-sm:h-16" onClick={() => setUserConnected(true)}><RiMessageLine /></button> }
             </div>
           </div>
-           <div className={`max-sm:w-full max-sm:absolute top-10 max-sm:p-10 w-full ${ userConnected ? "inset-0 bg-opacity-50 backdrop-blur" : ""} `}>
+           <div className={`max-sm:w-full max-sm:absolute max-sm:p-10 w-full ${ userConnected ? "inset-0 bg-opacity-50 backdrop-blur" : ""} `}>
            {
             userConnected ? (
             <>
