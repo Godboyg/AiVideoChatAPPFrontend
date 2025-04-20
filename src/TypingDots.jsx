2d49bf839eb8f02f0c13894536c1edebd72dcc38
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-20 09:01:07 +0530

    Update TypingDots.jsx

--- src/TypingDots.jsx
+++ src/TypingDots.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const TypingDots = () => {
   return (
-    <div className="flex gap-1 items-center justify-center h-6 pl-2">
-      <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:0s]"></span>
-      <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:0.2s]"></span>
-      <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:0.4s]"></span>
+    <div className="flex items-center justify-center h-6 pl-2">
+      <div className="flex gap-1 bg-white rounded-md">
+        <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:0s]"></span>
+        <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:0.2s]"></span>
+        <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:0.4s]"></span>
+      </div>
     </div>
   );
 };
